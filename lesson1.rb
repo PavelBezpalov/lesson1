66482fa89eac74a179b11776348ef81daaa311ea
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-10-08 18:44:34 +0300

    method 'age' implemented

--- lesson1.rb
+++ lesson1.rb
@@ -5,7 +5,19 @@ class Lesson1
   end
 
   def age(birthday)
-    # TODO
+    if birthday
+      current_date = Date.parse(Time.now.to_s)
+      birthday_date = Date.parse(birthday)
+      age_in_days = (current_date - birthday_date).to_i
+      age_in_years = age_in_days / 365
+      age_in_hours = age_in_days * 24
+      age_in_minutes = age_in_hours * 60
+      age_in_seconds = age_in_minutes * 60
+      return "Я живу #{age_in_years} года или #{age_in_days} дней или " \
+              "#{age_in_hours} часов или #{age_in_minutes} минут или " \
+              "#{age_in_seconds} секунд"
+    end
+    'Invalid Date Format'
   end
 
   def name
