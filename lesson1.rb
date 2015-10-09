15393cb88fb56f6201a23e8b5395de0d76e3006f
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-10-09 08:43:47 +0300

    Remove redundant variables

--- lesson1.rb
+++ lesson1.rb
@@ -9,13 +9,9 @@ class Lesson1
       current_date = Date.parse(Time.now.to_s)
       birthday_date = Date.parse(birthday)
       age_in_days = (current_date - birthday_date).to_i
-      age_in_years = age_in_days / 365
-      age_in_hours = age_in_days * 24
-      age_in_minutes = age_in_hours * 60
-      age_in_seconds = age_in_minutes * 60
-      return "Я живу #{age_in_years} года или #{age_in_days} дней или " \
-              "#{age_in_hours} часов или #{age_in_minutes} минут или " \
-              "#{age_in_seconds} секунд"
+      return "Я живу #{age_in_days / 365} года или #{age_in_days} дней или " \
+             "#{age_in_days * 24} часов или #{age_in_days * 24 * 60} минут " \
+             "или #{age_in_days * 24 * 60 * 60} секунд"
     end
     'Invalid Date Format'
   end
